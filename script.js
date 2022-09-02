daa6afaec6a8f9cd488c68ff863886112d068718
afinster <afinster11@gmail.com>
2022-09-02 15:40:05 -0500

    new adds

--- script.js
+++ script.js
@@ -2,21 +2,20 @@
 
 const { pickPlanet, addDestinationInfo } = require("./scriptHelper");
 
-window.addEventListener("load", ()=> {
-    const form = document.querySelector("form");
-    form.addEventListener("submit", (event)=>{
+window.addEventListener("load", function() {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event){
         event.preventDefault();
-        //List DOM
-        let pilot = document.querySelector("input[name=pilotName]").value;
-        let copilot = document.querySelector("input[name=copilotName]").value;
-        let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
-        let cargoLevel = document.querySelector("input[name=cargoMass]").value;
+        
+        let pilot = document.querySelector("input[name=pilotName]");
+        let copilot = document.querySelector("input[name=copilotName]");
+        let fuelLevel = document.querySelector("input[name=fuelLevel]");
+        let cargoLevel = document.querySelector("input[name=cargoMass]");
         let list = document.getElementById('faultyItems');
 
-        //use formsubmission to validate and update list
         formSubmission(document,list,pilot,copilot,fuelLevel,cargoLevel);
     })
-    
+
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse = myFetch();
