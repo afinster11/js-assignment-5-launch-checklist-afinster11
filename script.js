e6ef8a0fc4f32c5337306e73ac1f544cc70753c1
afinster <afinster11@gmail.com>
2022-09-06 13:16:55 -0500

    New changes from TA

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Write your JavaScript code here!
 
-//const { pickPlanet, addDestinationInfo } = require("./scriptHelper");
+const { pickPlanet, addDestinationInfo } = require("./scriptHelper");
 
 window.addEventListener("load", function() {
     
