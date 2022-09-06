7e5354c47280a25f3e7f3c72910d382c28bf0c9f
afinster <afinster11@gmail.com>
2022-09-06 13:05:49 -0500

    New changes from TA

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ const { pickPlanet, addDestinationInfo } = require("./scriptHelper");
 window.addEventListener("load", function() {
     let form = document.querySelector("form");
     form.addEventListener("submit", function(event){
-        event.preventDefault();
+        
         
         let pilot = document.querySelector("input[name=pilotName]");
         let copilot = document.querySelector("input[name=copilotName]");
         let fuelLevel = document.querySelector("input[name=fuelLevel]");
         let cargoLevel = document.querySelector("input[name=cargoMass]");
         let list = document.getElementById('faultyItems');
+        event.preventDefault();
 
         formSubmission(document,list,pilot,copilot,fuelLevel,cargoLevel);
     })
